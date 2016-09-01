f21dd76e3d0e2f07f8c2053d02bc5e1055e17043
NanaOKA <nana.abankwa@gmail.com>
2016-09-01 16:27:23 +0100

    1st commit

---
+++ main.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%
+% Numerical Time Domain Simulation
+% to investigate motions of a fishing vessel
+%
+% Nana Okra Abankwa
+% n.abankwa@soton.ac.uk
+% September 2016
+%%%%%%%%%%%%%%%%%%%%%
+
+clc       % clear all input and output from command window display
+clear     % clear all variables from workspace
+close all % close all open figure windows
