55f36817f797307ac99fa1a890df0a952ead4b93
NanaOKA <nana.abankwa@gmail.com>
2016-09-01 16:41:45 +0100

    Added dimensions for tank

--- main.m
+++ main.m
@@ -14,3 +14,9 @@ close all % close all open figure windows
 %% MISCELLANEOUS USER INPUT
 w_density = 1029;   % [kg/m^3] Density of water
 g = 9.81;           % [Newtons/kg] Gravitational force
+
+%% WORLD
+t_length = 30.0;    % Tank length [m]  default = 100 X axis
+t_width  = 30.0;    % Tank width [m]  default = 100 Y axis
+t_depth  = 30.0;    % Tank length [m] default = 100 Z axis 
+t_grid   = 0.05;    % Size of grid tank will be divided into [m]
